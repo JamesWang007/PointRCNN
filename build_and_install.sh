732f0b07071e485560022380a81abecc7250d2d1
JamesWang007 <wanghaiyang20@gmail.com>
2019-12-18 16:52:08 -0800

    a

--- build_and_install.sh
+++ build_and_install.sh
@@ -9,3 +9,7 @@ cd ../roipool3d/
 python setup.py install
 
 cd ../../../tools
+
+python eval_rcnn.py --cfg_file cfgs/default.yaml --ckpt PointRCNN.pth --batch_size 1 --eval_mode rcnn --set RPN.LOC_XZ_FINE False
+
+python eval_rcnn.py --cfg_file cfgs/default.yaml --ckpt ../output/rpn/ckpt/checkpoint_epoch_200.pth --batch_size 4 --eval_mode rcnn 
---
+++ tools/ex13.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 29 14:21:30 2019
+
+@author: james
+"""
+
+from sys import argv
+
+script, first, second, third = argv
+
+print ("The script is called:", script)
+print ("The first variable is:", first)
+print ("The second variable is:", second)
+print ("Your third variable is:", third)
